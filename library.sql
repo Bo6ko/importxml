63ef1904dcb6596cc82b9f7087564dfccd99d586
Bozhidar Shopov <b.shopov88@gmail.com>
2021-01-12 19:55:31 +0200

    Add database

---
+++ library.sql
@@ -0,0 +1,47 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.1
+-- Dumped by pg_dump version 13.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: books; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.books (
+    book_id integer NOT NULL,
+    book_author character varying(100) NOT NULL,
+    book_name character varying(100) NOT NULL
+);
+
+
+ALTER TABLE public.books OWNER TO postgres;
+
+--
+-- Name: books books_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books
+    ADD CONSTRAINT books_pkey PRIMARY KEY (book_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
